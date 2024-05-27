f4cc5ab2dc490afb018113b7e09c65ffd7c0f028
Bill Gichuru Chakairu <115233741+lizztt@users.noreply.github.com>
2024-05-27 20:13:17 +0300

    Revert "Switching the email system from sandbox to gmail"
    This reverts commit 937876683761b1b77bf98fd20de4d28dca6ef758.

--- config/jetstream.php
+++ config/jetstream.php
@@ -58,10 +58,9 @@ return [
     */
 
     'features' => [
-        // Features::termsAndPrivacyPolicy(),
-        // Features::profilePhotos(),
+        Features::termsAndPrivacyPolicy(),
+        Features::profilePhotos(),
         // Features::api(),
-        // Features::teams(['invitations' => true]),
         Features::accountDeletion(),
     ],
 
